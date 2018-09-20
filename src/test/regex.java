6a174b7fdc72cd5d7d27798ddb5fdbb4f40f6d20
JisuLim <sanola3@gmail.com>
2018-09-21 04:54:54 +0900

    regexsdafsdfsd

--- src/test/regex.java
+++ src/test/regex.java
@@ -2,21 +2,31 @@ package test;
 
 public class regex {
     public static void main(String[] args) {
-        String a = "Muziiasdasd";
-        String b = "Muzi@asdas";
-        String c = "s";
-        String d = "muzi";
-        String e = "qm1@muzi@2iai";
-        a = a.toLowerCase();
-        b = b.toLowerCase();
+        String a = "aapple";
+        String b = "apple22";
+        String c = "2apple2";
+        String d = "a2apple2";
+        String e = "a2apple2a";
+        String f = "a2apple";
+        String g = "apple";
+        String regex = "[^a-zA-Z]?apple[^a-zA-Z]?";
+        String regex2 = "(?<![a-zA-Z])?apple(?![a-zA-Z])?";
 
-        System.out.println(a.matches("^muzi*"));
-        System.out.println(b.matches("^[muzi]"));
-        System.out.println(c.matches("^[0-9]*$"));
-       // System.out.println(d.matches("^[[^a-zA-Z](muzi)[^a-zA-Z]]*$"));
-        System.out.println(e.matches("^([^a-zA-Z]*)muzi([^a-zA-Z]*)"));
+        System.out.println("false여야함 " + a.matches(regex));
+        System.out.println("true여야함 " + b.matches(regex));
+        System.out.println("true여야함 " + c.matches(regex));
+        System.out.println("true여야함 " + d.matches(regex));
+        System.out.println("true여야함 " + e.matches(regex));
+        System.out.println("true여야함 " + f.matches(regex));
+        System.out.println("true여야함 " + g.matches(regex));
         System.out.println();
-        System.out.println(a.toLowerCase().contains("Muzi".toLowerCase()));
-        System.out.println(b.toLowerCase().contains("Muzi".toLowerCase()));
+
+        System.out.println("false여야함 " + a.matches(regex2));
+        System.out.println("true여야함 " + b.matches(regex2));
+        System.out.println("true여야함 " + c.matches(regex2));
+        System.out.println("true여야함 " + d.matches(regex2));
+        System.out.println("true여야함 " + e.matches(regex2));
+        System.out.println("true여야함 " + f.matches(regex2));
+        System.out.println("true여야함 " + g.matches(regex2));
     }
 }
