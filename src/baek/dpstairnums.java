5ed8904b597005576e43c4a9a8d08fa348c528a8
JisuLim <sanola3@gmail.com>
2018-09-25 05:58:12 +0900

    dpdpdp

---
+++ src/baek/dpstairnums.java
@@ -0,0 +1,30 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class dpstairnums {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int[][] dp = new int[101][10];
+
+        for(int i=1; i< 10; i++)
+            dp[0][i] = 1;
+
+        for(int i=1; i< num; i++) {
+            for(int j=0; j<=9; j++) {
+                if(j-1 >= 0) dp[i][j] += dp[i-1][j-1];
+                if(j+1 <= 9) dp[i][j] += dp[i-1][j+1];
+                dp[i][j] %= 1000000000;
+            }
+        }
+        long answer = 0;
+        for(int i=0; i<=9; i++)
+            answer += dp[num-1][i];
+
+        System.out.println(Arrays.stream(dp[num-1]).sum());
+        System.out.println(answer%1000000000);
+    }
+}
