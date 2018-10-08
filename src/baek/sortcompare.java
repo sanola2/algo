cb8eed8f3073ed4599c26425b5f684faee7318e8
JisuLim <sanola3@gmail.com>
2018-10-09 07:10:55 +0900

    comperator

--- src/baek/sortcompare.java
+++ src/baek/sortcompare.java
@@ -1,10 +1,11 @@
-package test;
+package baek;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class noneed {
+public class sortcompare {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
