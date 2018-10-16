9563af8b01519d5a2b96f9044006e811b952a649
JisuLim <sanola3@gmail.com>
2018-10-16 23:05:28 +0900

    Finding cycles in a graph

--- src/test/noneed.java
+++ src/test/noneed.java
@@ -7,38 +7,27 @@ import java.util.*;
 public class noneed {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
-        int[][] array = new int[num][2];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i=0; i< num; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            array[i][0] = Integer.valueOf(st.nextToken());
-            array[i][1] = Integer.valueOf(st.nextToken());
-        }
-        Arrays.sort(array, (o1, o2) -> {
-            if(o1[0] == o2[0])
-                return Integer.compare(o1[1], o2[1]);
-            return Integer.compare(o1[0], o2[0]);
-        });
-        for(int i=0; i< num; i++) {
-            System.out.println(array[i][0] + " " + array[i][1]);
-        }
-    }
-    static void sort(int[][] array) {
-        for(int i=0; i< array.length-2; i++) {
-            for(int j=0; j< array.length-1 -i; j++) {
-                if(array[j][0] >= array[j+1][0]) {
-                    if(array[j][0] == array[j+1][0]) {
-                        if(array[j][1] < array[j+1][1])
-                            continue;
-                    }
-                    for (int k = 0; k < 2; k++) {
-                        int temp = array[j][k];
-                        array[j][k] = array[j + 1][k];
-                        array[j + 1][k] = temp;
-                    }
-                }
+        String A = st.nextToken();
+        int P = Integer.valueOf(st.nextToken());
+        LinkedList res = new LinkedList();
+        res.add(Integer.valueOf(A));
+        int sum;
+
+        while (true) {
+            sum = 0;
+            String[] numbers = A.split("");
+            for(int i=0; i< numbers.length; i++)
+                sum += Math.pow(Integer.valueOf(numbers[i]), P);
+
+            A = String.valueOf(sum);
+
+            if(res.contains(sum)) {
+                System.out.println(res.indexOf(sum));
+                return;
             }
+            res.add(sum);
         }
     }
 }
